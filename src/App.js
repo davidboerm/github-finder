82fe122b26de96eeb7d53bf56e249a68a0fde476
davidboerm <davidboerm@gmail.com>
2021-12-27 19:58:00 -0600

    add UserResults comp, Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import NotFound from './pages/NotFound'
-import Footer from './components/layout/Footer';
-import Navbar from './components/layout/Navbar'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
@@ -11,20 +11,17 @@ function App() {
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
 
-        <main className='container mx-auto px-3 pb-12'>
+        <main className="container mx-auto px-3 pb-12">
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/notfound' element={<NotFound />} />
-            <Route path='/*' element={<NotFound />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
           </Routes>
         </main>
 
         <Footer />
       </div>
-      
-      
-      
     </Router>
   );
 }
