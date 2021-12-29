1da03f506bfdd23d3f6d2545ec9aebea0cdc05cf
davidboerm <davidboerm@gmail.com>
2021-12-28 21:50:31 -0600

    added GithubReducer, modified GithubContext

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -11,8 +11,6 @@ function UserResults() {
     fetchUsers();
   }, []);
 
-  
-
   if (!loading) {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
